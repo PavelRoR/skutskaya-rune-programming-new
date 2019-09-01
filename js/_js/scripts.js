d9fa096bae5cc2c095c2eada5aa88e7a93b7ef20
pavelp21 <pavelvp2182@yandex.ru>
2019-09-01 23:18:03 +0500

    Crossbrowsering

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             var head = document.getElementsByTagName("head")[0];
             var link = document.createElement("link");
             link.rel = "stylesheet";
-            link.href = "css/ie.css";
+            link.href = "css/ie.min.css";
             head.appendChild(link);
         }
     });
